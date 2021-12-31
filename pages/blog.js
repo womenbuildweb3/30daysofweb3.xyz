b8b8539c14cb90c907c5800d930bc5f4de86b78b
somarcamila <camirgarzon@gmail.com>
2021-12-30 20:33:24 -0800

    redirect to hashnode

--- pages/blog.js
+++ pages/blog.js
@@ -3,18 +3,19 @@ import client from "../apollo-client";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
+import Link from "next/link";
 
 export default function Blog({ myData }) {
   return (
     // <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-black">
     <div className={styles.container}>
       <Navbar />
-      <div className="max-w-3xl mx-auto pt-6 flex flex-col justify-center items-start max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pb-60">
+      <div className="max-w-3xl mx-auto pt-6 flex flex-col justify-center items-start max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pb-80">
         {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
         <h1 className="pb-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold text-3xl md:text-5xl tracking-tight">
           Blog
         </h1>
-        <p className="mb-4 text-gray-400">
+        <p className="mb-4 text-lg text-gray-400">
           {" "}
           One of my 2022 goals is to put out more written content! You can
           expect blogs on the following topics:{" "}
@@ -57,9 +58,11 @@ export default function Blog({ myData }) {
           </h1>
           {myData.publication.posts.map((post) => (
             <div className="flex flex-col text-gray-500">
-              <h1 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+                <Link href="https://camiinthisthang.hashnode.dev/">
+              <h1 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100 select-none">
                 {post.title}
               </h1>
+              </Link>
               <p className="text-gray-400 pb-4">{post.brief}</p>
             </div>
           ))}
