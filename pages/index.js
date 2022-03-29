8dbe19960f9f49d17a0975f78d73d10c5ea74e0b
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-03-29 13:46:45 -0700

    new homepage, some about content

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,15 +15,41 @@ export default function Home() {
       <Navbar />
 
       <main>
-
           <header className={styles.homeHeroContainer}>
-              <h1 className={styles.homeHeroTitle}>
-                Women<br/>Build<br/>Web3
-              </h1>
-              <p className={styles.homeHeroSubtitle}>
-                Unlocking a new wave of builders around the globe by providing education, funding and opportunities.
-              </p>
+            <h1 className={styles.homeHeroTitle}>
+              LEARN.
+            </h1>
+            <h1 className={styles.homeHeroTitle}>
+              BUILD.
+            </h1>
+            <h1 className={styles.homeHeroTitle}>
+              EARN.
+            </h1>
+          
+          <Link href="/contact" passHref>
+            <button className={styles.contactButton}>
+              Contact Us
+            </button>
+          </Link>
+          <Link href="/blog" passHref>
+            <button className={styles.blogButton}>
+              Read Our Blog
+            </button>
+          </Link>
         </header>
+        
+        <section className={styles.homeSection}>
+          <h2>What We Do</h2>
+          <ul className={styles.homeSkillsList}>
+            <li className={styles.homeSkill}>Developer Relations</li>
+            <li className={styles.homeSkill}>Smart Contracts</li>
+            <li className={styles.homeSkill}>Frontend Development</li>
+            <li className={styles.homeSkill}>Content Creation</li>
+            <li className={styles.homeSkill}>Hackathons</li>
+            <li className={styles.homeSkill}>Public Goods</li>
+          </ul>
+        </section>
+
       </main>
       <Footer/>
     </div>
