424fca629157c1a162fd09d68232571753c18b47
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-04-02 18:16:07 -0700

    style: gray background, new homepage content

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/About.module.css";
 import Navbar from "../components/Navbar";
 // import { SocialIcon } from "react-social-icons";
 // import Link from "next/link";
@@ -8,7 +8,7 @@ import Footer from "../components/Footer";
 
 export default function About() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>About | Women Build Web3</title>
         <meta name="description" content="Providing education, oppportunities, and funding to a new wave of web3 builders" />
@@ -16,9 +16,7 @@ export default function About() {
       </Head>
       <Navbar />
       <main>
-        <h1 className="lt-pink-text">
-          About
-        </h1>
+        <h1>About</h1>
         <p>
           Women Build Web3 (WBW3) is a global collective of women and non-binary developers learning about and working in web3.
           We are frontend, backend, and full-stack engineers, designers, content creators, and more.
@@ -29,6 +27,6 @@ export default function About() {
         </p>
       </main>
       <Footer/>
-    </div>
+    </>
   );
 }
