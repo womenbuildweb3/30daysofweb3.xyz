446f0fded3a7c7d84082c2848d10d5203be148a1
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-05-12 14:03:20 -0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-DC5Z3247BE", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
