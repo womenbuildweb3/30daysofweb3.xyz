c505de4e0228576898bd939f722a5a6e36248423
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-06-10 08:28:19 -0700

    test fix for patrick's img

--- components/Speakers.js
+++ components/Speakers.js
@@ -11,7 +11,7 @@ const people = [
   {
     name: "Patrick Collins",
     title: "Chainlink",
-    imageUrl: "/images/Patrick.jpeg",
+    imageUrl: "/images/patrick.jpeg",
   },
   {
     name: "Ally Haire",
