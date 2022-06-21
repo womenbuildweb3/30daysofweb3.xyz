ea0b990867aa9a814f87c1e4f123cb614cc7d595
Avni1802 <avani1agrawal@gmail.com>
2022-06-21 09:37:45 +0530

    fixed redirection issue

--- pages/curriculum/[category]/index.js
+++ pages/curriculum/[category]/index.js
@@ -1,48 +1,15 @@
 import { useEffect, useState } from "react";
-import Navbar from "../../../components/Navbar";
-import Link from "next/link";
-import Image from "next/image";
-import Head from "next/head";
-import Footer from "../../../components/Footer";
-import Layout from "../../../components/Layout";
-import CurricSidebar from "../../../components/CurricSideBar";
-import CurricLayout from "../../../components/CurricLayout";
+import Router from "next/router";
 import getCurricContent from "../../../utils/curriculum";
 import getAllPostIds from "../../../utils/getAllPostIds";
 // import getCoursePaths from "../../../utils/getCoursePaths";
-import getCourseNavigationTree from "../../../utils/getCourseNavigationTree";
 
-export default function Course({ curricData, id, paths, navigationTree }) {
-  const [preferedColorScheme, setPreferedColorScheme] = useState("light");
-  // console.log(navigationTree, paths);
-
+export default function Course({ category }) {
   useEffect(() => {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      setPreferedColorScheme("dark");
-    }
-  }, []);
+    if (category) Router.push(`${category}/0-overview`);
+  }, [category]);
 
-  return (
-    <CurricLayout>
-      <CurricSidebar curricData={curricData} id={id} paths={paths} />
-      <Head>
-        <title>30 Days of Web3 | Curriculum </title>
-        <meta
-          name="description"
-          content="Providing education, oppportunities, and funding to a new wave of web3 builders"
-        />
-        {preferedColorScheme === "light" && (
-          <link rel="icon" href="/favicon.ico" />
-        )}
-        {preferedColorScheme === "dark" && (
-          <link rel="icon" href="/favicon-white.ico" />
-        )}
-      </Head>
-    </CurricLayout>
-  );
+  return <></>;
 }
 
 export async function getStaticPaths() {
@@ -58,7 +25,6 @@ export async function getStaticProps({ params }) {
   // console.log(category);
   const curricData = getCurricContent(category, "0-overview");
   const paths = getAllPostIds();
-  const navigationTree = getCourseNavigationTree();
   return {
     // redirect: {
     //   permanent: false,
@@ -67,7 +33,7 @@ export async function getStaticProps({ params }) {
     props: {
       curricData,
       paths,
-      navigationTree,
+      category,
     },
   };
 }
--- pages/curriculum/index.js
+++ pages/curriculum/index.js
@@ -1,4 +1,11 @@
+import React, { useEffect, useState } from "react";
+import Router from "next/router";
+
 export default function Course() {
+  useEffect(() => {
+    Router.push("curriculum/0-getting-started/0-overview");
+  }, []);
+
   return <></>;
 }
 
