9e967e7c310cd7b4b9c8761badbc278cfd98367b
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-06-20 20:11:50 -0700

    comment out redirect

--- pages/course/index.js
+++ pages/course/index.js
@@ -2,11 +2,11 @@ export default function Course() {
   return <></>;
 }
 
-export async function getStaticProps() {
-  return {
-    redirect: {
-      permanent: false,
-      destination: `course/0-getting-started/0-overview`,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   return {
+//     redirect: {
+//       permanent: false,
+//       destination: `course/0-getting-started/0-overview`,
+//     },
+//   };
+// }
