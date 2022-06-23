c0318d72af33d61b163ca01dcb9c1c00e5ca521f
Avni1802 <avani1agrawal@gmail.com>
2022-06-24 00:54:09 +0530

    added next on curriculum pages

--- components/CurriculumContent.js
+++ components/CurriculumContent.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Navbar from "./Navbar";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "github-markdown-css";
 import { getNextLesson } from "../utils/lessons";
+import navigation from "../utils/navigation.json";
 
-const CurriculumContent = ({ curricData, id, paths }) => {
-  // const next = getNextLesson(id, paths);
-
+const CurriculumContent = ({ curricData }) => {
+  const router = useRouter();
+  const { category, subCategory } = router.query;
+  const nextPath = getNextLesson(category, subCategory, navigation);
+  console.log(nextPath);
   return (
     <div className="bg-white px-4 py-16">
       <ReactMarkdown
@@ -17,13 +21,13 @@ const CurriculumContent = ({ curricData, id, paths }) => {
       >
         {curricData.content}
       </ReactMarkdown>
-      {/* <div>
-        <Link href={next}>
-          <button className="flex justify-center bg-indigo-800 w-full rounded-md p-2">
-            Next
+      <div className="flex justify-center w-full mt-5">
+        <Link href={nextPath}>
+          <button className="flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-royal-600 hover:bg-royal-700 md:py-3 md:text-lg md:px-8">
+            {nextPath == "/" ? "Finish. Let's Tweet!" : "Next"}
           </button>
         </Link>
-      </div> */}
+      </div>
     </div>
   );
 };
--- utils/curriculum.js
+++ utils/curriculum.js
@@ -3,19 +3,23 @@ import path from "path";
 import matter from "gray-matter";
 
 export default function getCurricContent(folderName = null, fileName) {
-  const postsDirectory = path.join(process.cwd(), "curriculum", folderName);
+  try {
+    const postsDirectory = path.join(process.cwd(), "curriculum", folderName);
 
-  // Read markdown file as string
-  const fullPath = path.join(postsDirectory, `${fileName}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, `${fileName}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
 
-  const curriculum = {
-    data: matterResult.data,
-    content: matterResult.content,
-  };
+    const curriculum = {
+      data: matterResult.data,
+      content: matterResult.content,
+    };
 
-  return curriculum;
+    return curriculum;
+  } catch (err) {
+    return null;
+  }
 }
