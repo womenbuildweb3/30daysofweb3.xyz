92b0b3fc44d692dca1a0a61953ec0481778f3495
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-06-23 18:03:49 -0700

    fix

--- components/TweetButton.js
+++ components/TweetButton.js
@@ -1,6 +1,6 @@
 const TweetButton = ({copy}) => {
     const tweet = encodeURIComponent(copy)
-    const url = `https://twitter.com/intent/tweet?text=${copy}`
+    const url = `https://twitter.com/intent/tweet?text=${tweet}`
     return (
         <a href={url} target="_blank" rel="noreferrer">
           <button className="flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-royal-600 hover:bg-royal-700 md:py-3 md:text-lg md:px-8">
